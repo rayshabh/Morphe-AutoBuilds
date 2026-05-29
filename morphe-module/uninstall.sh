e08279fbf48e35a617c95fd0c966110ec81397cf
rayshabh <khargareeshav@gmail.com>
2026-05-30 01:17:02 +0530

    Update uninstall.sh

--- morphe-module/uninstall.sh
+++ morphe-module/uninstall.sh
@@ -3,7 +3,7 @@
 MODDIR=${0%/*}
 . "$MODDIR/config"
 
-rm -f "/data/adb/rvelo/${MODDIR##*/}.apk"
-rmdir "/data/adb/rvelo"
+rm -f "/data/adb/morphe/${MODDIR##*/}.apk"
+rmdir "/data/adb/morphe"
 
 rm -f "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
